65e0f8f16e54f9452e80a06f744529cc4190dc81
Piotr Badełek <piotrekbadelek@gmail.com>
2021-02-05 19:26:48 +0100

    Create index.php

---
+++ basic-project-structure/index.php
@@ -0,0 +1,21 @@
+<?php include_once "./kasztan.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hello World!</title>
+</head>
+
+<body>
+	<?php
+	Kasztan::view("ExampleView");
+	Kasztan::component("ExampleComponent");
+	?>
+
+	<iframe src="<?php Kasztan::asset("ExampleAsset.html") ?>" frameborder="0"></iframe>
+</body>
+
+</html>
